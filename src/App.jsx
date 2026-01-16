82da61fa12534d2407a5a40c83da2884805af8f1
Simone <info@simoneicardi.com>
2026-01-16 12:09:13 +0100

    crea footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import MainHeader from "./components/MainHeader"
 import MainContent from "./components/MainContent"
+import MainFooter from "./components/MainFooter"
 
 function App() {
     return (
         <>
             <MainHeader />
             <MainContent />
+            <MainFooter />
         </>
     )
 }
