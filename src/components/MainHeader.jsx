d8dbdc3bde10bdc212303cd19eaa51502ff11b07
Simone <info@simoneicardi.com>
2026-01-16 11:53:21 +0100

    completa componente header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,6 +1,22 @@
+
+const title = "Ciao sono un titolo dinamico dell'Header!!";
+
+
 const MainHeader = () => {
     return (
-        <h2>Ciao sono il tuo Header!!!</h2>
+        <header>
+            <figure>
+                <img src="/logo.png" alt="Logo" />
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#" className="active">Prodotti</a></li>
+                    <li><a href="#">Chi Siamo</a></li>
+                    <li><a href="#">Contatti</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
